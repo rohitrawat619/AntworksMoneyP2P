5f56eff8ac33ea13237d572c24d0baa026d2a3fd
Dheeraj Dutta <dheeraj.dutta@antworksmoney.com>
2024-01-23 14:55:21 +0530

    LendSocial Dashboard (Borrower Module) Updated Code

--- application/modules/surgeModule/controllers/Borrower.php
+++ application/modules/surgeModule/controllers/Borrower.php
@@ -6,14 +6,15 @@ class Borrower extends CI_Controller
 	{
 		parent::__construct();
 		
-		$this->load->model(array('P2pborrowermodel', 'Borroweraddmodel'));
-		$this->load->model('Basicfiltermodel');
+		$this->load->model(array('P2pborrowermodel', 'Borroweraddmodel', 'disbursementModel','Basicfiltermodel'));
 		$this->load->helper('url');
-		$this->cldb = $this->load->database('new_p2p_sandbox', TRUE);
+		//$this->cldb = $this->load->database('new_p2p_sandbox', TRUE);
+		$this->cldb = $this->load->database('credit-line', TRUE);
 		//$this->load->model('Common_model');
 		$this->load->library('form_validation');
 		$this->load->helper('custom');
 		$this->load->library('pagination');
+        
 		
 		error_reporting(0);
 					// $database_name = $this->cldb->database;
@@ -22,6 +23,7 @@ class Borrower extends CI_Controller
 		$this->partner_id = $this->session->userdata('partner_id');
 		//	print_r($this->session->userdata());
 		$this->paginationUrl = base_url() . "surgeModule/borrower/";
+		$this->baseUrl = $this->paginationUrl;
 	}
 	
 
@@ -446,6 +448,217 @@ class Borrower extends CI_Controller
 		   exit;
 	   }	
 	}
+	
+	
+	
+	
+	//*************disburse management**********//
+	    public function disburse_pending_list(){
+        $disburseRequest =1;
+        $status = NULL; 
+    
+        $config = array(
+            "base_url" => $this->baseUrl . "disburse_pending_list",
+            "total_rows" => $this->disbursementModel->getCountDisburseListStatus($disburseRequest, $status),
+            "per_page" => 20,
+            "uri_segment" => 4
+        );
+    
+        $this->pagination->initialize($config);
+        $page = $this->uri->segment(4) ? $this->uri->segment(4) : 0;
+    
+        $data["links"] = $this->pagination->create_links();   
+        $data['lists'] = $this->disbursementModel->getDisburseRequestList($config["per_page"], $page, $disburseRequest, $status);
+    
+        // Debugging
+        // echo "<pre>";
+        // print_r($config);
+        // die();
+    
+        $data['page'] = $page;
+		 $this->load->view('template-surgeModule/header');
+		$this->load->view('template-surgeModule/nav');
+        $this->load->view('borrower/disbursement_pending_list', $data);
+		$this->load->view('template-surgeModule/footer');
+    }
+    
+    public function update_disburse_status(){
+        $ids = $this->input->post('ids');
+        $loanStatus = $this->input->post('status');
+        $amount = $this->input->post('amount');
+        $roi = $this->input->post('roi');
+        // echo $ids."!".$loanStatus;die();
+        if($loanStatus==3){ // generate_bank_file 
+            
+            $csv_data = $this->generate_bank_file_excel($ids);
+            // echo $csv_data;die();
+            $update_disburse_status['query'] =	$this->disbursementModel->update_disburse_status($loanStatus, $ids,$amount,$roi);
+        if ($update_disburse_status) {
+            $update_disburse_status['status'] = 1;
+            $update_disburse_status['message'] = 'Update successful';
+        } else {
+            $update_disburse_status['status'] = 0;
+            $update_disburse_status['message'] = 'Update Failed';
+        }
+        
+        $update_disburse_status['csv_data'] = $csv_data;
+        echo json_encode($update_disburse_status);
+        die();
+                /******ending of generate bank file function use**********/
+        }
+        
+        else{
+        $update_disburse_status = $this->disbursementModel->update_disburse_status($loanStatus, $ids,$amount,$roi);
+        
+        if ($update_disburse_status) {
+            echo json_encode(array('status' => 1, 'message' => 'Update successful'));
+        } else {
+            echo json_encode(array('status' => 0, 'message' => 'Update failed'));
+        }
+        die();
+    }
+    }
+
+    public function generate_bank_file_excel($LoanId){
+                // $LoanId = 2;
+                $respResultSet = $this->disbursementModel->generate_bank_file_excel($LoanId);
+           //     print_r($respResultSet); die();
+        $this->load->dbutil();
+       $this->load->helper('file');
+       $this->load->helper('download'); 
+       $delimiter = ",";
+       $newline = "\r\n";
+       
+       $projectName = "Bank_file";
+       $start_date = "";
+       $end_date="";
+       //print_r($respResultSet);
+       if($respResultSet){
+               $filename = $projectName."__product_from-".$start_date." To-".$end_date.".csv";
+              //	echo $respResultSet;
+           $data = $this->dbutil->csv_from_result($respResultSet, $delimiter, $newline);
+       	// force_download($filename, $data);
+               return  $data;
+           }else{
+               return "hello data";
+           }
+   }
+
+
+	public function disburse_list_generate_bank_file(){
+        $disburseRequest =1;
+        $status = 1;
+		$config = array();
+		$config["base_url"] = $this->baseUrl . "disburse_list_generate_bank_file";
+		$config["total_rows"] = $this->disbursementModel->getCountDisburseListStatus($disburseRequest,$status);
+		$config["per_page"] = 20;
+		$config["uri_segment"] = 4;
+		$this->pagination->initialize($config);
+		$page = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0;
+		// echo $this->uri->segment(4);die();
+		$data["links"] = $this->pagination->create_links();	
+			
+		 $this->load->view('template-surgeModule/header');
+		 $this->load->view('template-surgeModule/nav');
+		$data['lists'] = $this->disbursementModel->getDisburseRequestList($config["per_page"], $page,$disburseRequest,$status);
+        // echo "<pre>";print_r($data);die();
+		$data['page'] = $page;
+		$this->load->view('borrower/disbursement_generate_bank_file',$data);
+		
+		 $this->load->view('template-surgeModule/footer');
+    }
+
+
+    public function disburse_list_under_process(){
+        $disburseRequest =1;
+        $status = 3;
+$config["base_url"] =$this->baseUrl . "disburse_list_under_process";
+$config["total_rows"] = $this->disbursementModel->getCountDisburseListStatus($disburseRequest,$status);
+$config["per_page"] = 20;
+$config["uri_segment"] = 4;
+$this->pagination->initialize($config);
+$page = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0;
+
+$data["links"] = $this->pagination->create_links();	
+
+$this->load->view('template-surgeModule/header');
+$this->load->view('template-surgeModule/nav');
+$data['lists'] = $this->disbursementModel->getDisburseRequestList($config["per_page"], $page,$disburseRequest,$status);
+$data['page'] = $page;
+// echo "<pre>";print_r($data);die();
+$this->load->view('borrower/disburse_list_under_process',$data);
+
+ $this->load->view('template-surgeModule/footer');    
+    }
+
+
+    public function disburse_list_disbursed(){
+        $disburseRequest =1;
+        $status = 4;
+		$config = array();
+		$config["base_url"] = $this->baseUrl . "disburse_list_disbursed";
+		$config["total_rows"] = $this->disbursementModel->getCountDisburseListStatus($disburseRequest,$status);
+		$config["per_page"] = 20;
+		$config["uri_segment"] = 4;
+		$this->pagination->initialize($config);
+		$page = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0;
+		// echo $this->uri->segment(4);die();
+		$data["links"] = $this->pagination->create_links();	
+			
+		$this->load->view('template-surgeModule/header');
+		 $this->load->view('template-surgeModule/nav');
+		$data['lists'] = $this->disbursementModel->getDisburseRequestList($config["per_page"], $page,$disburseRequest,$status);
+        // echo "<pre>";print_r($config);die();
+		$data['page'] = $page;
+		$this->load->view('borrower/disbursement_list',$data);
+		$this->load->view('template-surgeModule/footer');
+    }
+
+
+    public function disburse_pending_rejected_list(){
+        $disburseRequest =1;
+        $status = 2;
+		$config = array();
+		$config["base_url"] = $this->baseUrl . "disburse_list_disbursed";
+		$config["total_rows"] = $this->disbursementModel->getCountDisburseListStatus($disburseRequest,$status);
+		$config["per_page"] = 20;
+		$config["uri_segment"] = 4;
+		$this->pagination->initialize($config);
+		$page = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0;
+		// echo $this->uri->segment(4);die();
+		$data["links"] = $this->pagination->create_links();	
+			
+		$this->load->view('template-surgeModule/header');
+		$this->load->view('template-surgeModule/nav');
+		$data['lists'] = $this->disbursementModel->getDisburseRequestList($config["per_page"], $page,$disburseRequest,$status);
+        // echo "<pre>";print_r($config);die();
+		$data['page'] = $page;
+		$this->load->view('borrower/disburse_pending_rejected_list',$data);
+		$this->load->view('template-surgeModule/footer');
+    }
+
+    public function disbursement_under_process_rejected_list(){
+        $disburseRequest =1;
+        $status = 5;
+		$config = array();
+		$config["base_url"] = $this->baseUrl . "disburse_list_disbursed";
+		$config["total_rows"] = $this->disbursementModel->getCountDisburseListStatus($disburseRequest,$status);
+		$config["per_page"] = 20;
+		$config["uri_segment"] = 4;
+		$this->pagination->initialize($config);
+		$page = ($this->uri->segment(4)) ? $this->uri->segment(4) : 0;
+		// echo $this->uri->segment(4);die();
+		$data["links"] = $this->pagination->create_links();	
+			
+		$this->load->view('template-surgeModule/header');
+		$this->load->view('template-surgeModule/nav');
+		$data['lists'] = $this->disbursementModel->getDisburseRequestList($config["per_page"], $page,$disburseRequest,$status);
+        // echo "<pre>";print_r($config);die();
+		$data['page'] = $page;
+		$this->load->view('borrower/disbursement_under-process_rejected_list',$data);
+		$this->load->view('template-surgeModule/footer');
+    }
+	
 
 
 }
---
+++ application/modules/surgeModule/models/disbursementModel.php
@@ -0,0 +1,127 @@
+<?php
+class disbursementModel extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+		//$this->cldb = $this->load->database('new_p2p_sandbox', TRUE);
+      $this->cldb = $this->load->database('credit-line', TRUE);
+    }
+
+
+    
+    public function getCountDisburseListStatus($disburseRequest, $status)
+{
+    $this->cldb->select('COUNT(*) as count');
+    $this->cldb->from('p2p_loan_list as a');
+    $this->cldb->join('p2p_borrowers_list as b', 'a.borrower_id = b.id', 'LEFT');
+
+    
+    if (!empty($status)) {
+        $this->cldb->where_in("a.status", $status);
+    } else {
+        
+        $this->cldb->where("a.status IS NULL");
+    }
+
+    return $this->cldb->count_all_results();
+}
+
+
+
+
+    public function getDisburseRequestList($limit, $start, $disburseRequest,$status)
+{
+    $this->cldb->select('*, a.status, a.id');
+    $this->cldb->from('p2p_loan_list as a');
+    $this->cldb->join('p2p_borrowers_list as b', 'a.borrower_id = b.id', 'LEFT');
+    $this->cldb->where_in("a.disbursement_request", $disburseRequest);
+    // Check if the values array is not empty
+    if (!empty($status)) {
+        $this->cldb->where_in("a.status", $status);
+    } else {
+        
+        $this->cldb->where("a.status IS NULL");
+    }
+
+    $query = $this->cldb->limit($limit, $start);
+
+    return $res = $query->get()->result_array();
+}
+    
+
+    public function update_disburse_status($loanStatus, $ids,$amount,$roi) {
+        $data = array(
+            "status" => $loanStatus,
+            
+        );
+        if($loanStatus==3){// 3:status is for  generated bank files 
+        $idsArray = explode(",", urldecode($ids));
+
+					// Remove single quotes from each element in the array
+					$idsArray = array_map('trim', $idsArray);
+
+					// You may want to remove empty values if any
+					$ids = array_filter($idsArray);
+                }
+        else if($loanStatus==1){
+            $data['approved_loan_amount']=$amount;
+            $data['approved_interest']=$roi;
+            $data['disburse_amount']=$amount;
+        }
+        else if($loanStatus==4){
+            $data['disbursement_date']=date('Y-m-d h:i:s');
+            $data['loan_status']=1;
+        }
+        $this->cldb->where_in("id", $ids);
+        $query = $this->cldb->update("p2p_loan_list", $data);
+    
+    //    echo $this->cldb->last_query(); die();
+        if ($query) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+
+    public function generate_bank_file_excel($ids)
+				{
+					$idsArray = explode(",", urldecode($ids));
+
+					// Remove single quotes from each element in the array
+					$idsArray = array_map('trim', $idsArray);
+
+					// You may want to remove empty values if any
+					$idsArray = array_filter($idsArray);
+					
+					$this->cldb->select('
+					a.approved_loan_amount, 
+			"'.ANTWORKS_BANK_AC.'" as Debited_Account_No,
+			 c.ifsc_code as IFSC_CODE,
+			 c.account_no as Benificiary_AC_No,
+			 b.name as Benificiary_Name,
+			 "" as Sender_nd_Receiver_Information,
+					"Antworks P2P" as Sender_Name
+			
+			 '
+			 );
+					
+					$this->cldb->from('p2p_loan_list as a');
+					$this->cldb->join('p2p_borrowers_list as b', 'a.borrower_id =b.id', 'LEFT');
+					$this->cldb->join('p2p_borrower_bank_res AS c', 'c.borrower_id = b.id', 'LEFT');
+							// $this->cldb->join('invest_scheme_details as d', 'a.scheme_id =d.id', 'LEFT');
+							// $this->cldb->join('invest_vendors1 as e', 'b.vendor_id =e.VID', 'LEFT');
+					
+					 $this->cldb->where_in('a.id',$idsArray);
+					// $this->cldb->GROUP_BY("a.reinvestment_id");
+					// $this->cldb->ORDER_BY('a.reinvestment_id', 'desc');
+					$res = $this->cldb->get();
+
+					return $res; // $result = $res->result_array();
+				}
+
+
+}
+?>
